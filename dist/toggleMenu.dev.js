457bcee48026fa1e51904cea78b65a47e070765d
Jeanette Yue <yue.jeanette@gmail.com>
2020-03-15 12:51:26 -0700

    event handling for clicking hamburger and close icons

---
+++ dist/toggleMenu.dev.js
@@ -0,0 +1,33 @@
+"use strict";
+
+// toggle visibility of header nav and close button when visible (user is on mobile)
+var menu = document.querySelector(".mobile-menu__hamburger");
+var nav = document.querySelector(".header__nav");
+var close = document.querySelector(".mobile-menu__close");
+
+var show = function show(el, zIndex) {
+  el.style.opacity = "1";
+  el.style.zIndex = "".concat(zIndex);
+};
+
+var hide = function hide(el) {
+  el.style.opacity = "0";
+  el.style.zIndex = "-1";
+};
+
+var showNav = function showNav() {
+  show(nav, 1);
+  show(close, 2);
+};
+
+var hideNav = function hideNav() {
+  hide(close);
+  hide(nav);
+};
+
+menu.addEventListener("click", function () {
+  return showNav();
+});
+close.addEventListener("click", function () {
+  return hideNav();
+});
---
+++ toggleMenu.js
@@ -0,0 +1,28 @@
+// toggle visibility of header nav and close button when visible (user is on mobile)
+
+const menu = document.querySelector(".mobile-menu__hamburger");
+const nav = document.querySelector(".header__nav");
+const close = document.querySelector(".mobile-menu__close");
+
+const show = (el, zIndex) => {
+  el.style.opacity = "1";
+  el.style.zIndex = `${zIndex}`;
+};
+
+const hide = el => {
+  el.style.opacity = "0";
+  el.style.zIndex = "-1";
+};
+
+const showNav = () => {
+  show(nav, 1);
+  show(close, 2);
+};
+
+const hideNav = () => {
+  hide(close);
+  hide(nav);
+};
+
+menu.addEventListener("click", () => showNav());
+close.addEventListener("click", () => hideNav());
